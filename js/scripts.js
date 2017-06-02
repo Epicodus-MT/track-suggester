1355cdc90a4a3dfcad63e00d9483e5c6e38c1321
Mara Timberlake <maratimberlake@msn.com>
2017-06-02 08:59:12 -0700

    show intro text and buttons for individual languages

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+function myRuby() {
+    var button = document.getElementById('ruby');
+    if (button.style.display === 'none') {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
+
+function myPHP() {
+    var button = document.getElementById('php');
+    if (button.style.display === 'none') {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
+
+function myJava() {
+    var button = document.getElementById('java');
+    if (button.style.display === 'none') {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
+
+function myCSS() {
+    var button = document.getElementById('css');
+    if (button.style.display === 'none') {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
+
+function myCNet() {
+    var button = document.getElementById('cnet');
+    if (button.style.display === 'none') {
+        button.style.display = 'block';
+    } else {
+        button.style.display = 'none';
+    }
+}
