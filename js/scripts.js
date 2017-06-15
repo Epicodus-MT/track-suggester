e638ea30530da405b611b795e15475fbb53c6b8c
Mara Timberlake <maratimberlake@msn.com>
2017-06-14 21:28:06 -0700

    buttons for info-clocks hide block text, update quiz with first result

--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,86 @@
-function myRuby() {
-    var button = document.getElementById('ruby');
-    var hidden = false;
-    if (button.style.display === 'none') {
-        button.style.display = 'block';
-    } else {
-        button.style.display = 'none';
+//Front END
+
+
+$(document).ready(function() {
+  var resultLabel = $('#quiz-result');
+
+  $("#form1").submit(function(event) {
+    event.preventDefault();
+    var location = $("input:radio[name=location]:checked").val();
+    var development = $("input:radio[name=development]:checked").val();
+    var month = $("input:radio[name=month]:checked").val();
+    var language = $("input:radio[name=language]:checked").val();
+    var company = $("input:radio[name=company]:checked").val();
+
+
+    console.log({
+      'location': location,
+      'development': development,
+      'month': month,
+      'language': language,
+      'company': company
+    });
+
+    var resultText = '';
+    if (development === 'front') {
+      resultText = 'Be Design';
+    } else /* back */ {
+      resultText = 'Do Not be design';
     }
+
+    $('#inputs')[0].style.display = 'none';
+    $('#output')[0].style.display = 'block';
+    $('#quiz-result')[0].innerText = resultText;
+  })
+});
+
+function myRuby() {
+  var button = document.getElementById('ruby');
+  if (button.style.display === 'none') {
+    button.style.display = 'block';
+  } else {
+    button.style.display = 'none';
+  }
 }
 
 function myPHP() {
-    var button = document.getElementById('php');
-    if (button.style.display === 'none') {
-        button.style.display = 'block';
-    } else {
-        button.style.display = 'none';
-    }
+  var button = document.getElementById('php');
+  if (button.style.display === 'none') {
+    button.style.display = 'block';
+  } else {
+    button.style.display = 'none';
+  }
 }
 
 function myJava() {
-    var button = document.getElementById('java');
-    if (button.style.display === 'none') {
-        button.style.display = 'block';
-    } else {
-        button.style.display = 'none';
-    }
+  var button = document.getElementById('java');
+  if (button.style.display === 'none') {
+    button.style.display = 'block';
+  } else {
+    button.style.display = 'none';
+  }
 }
 
 function myCSS() {
-    var button = document.getElementById('css');
-    if (button.style.display === 'none') {
-        button.style.display = 'block';
-    } else {
-        button.style.display = 'none';
-    }
+  var button = document.getElementById('css');
+  if (button.style.display === 'none') {
+    button.style.display = 'block';
+  } else {
+    button.style.display = 'none';
+  }
 }
 
 function myCNet() {
-    var button = document.getElementById('cnet');
-    if (button.style.display === 'none') {
-        button.style.display = 'block';
-    } else {
-        button.style.display = 'none';
-    }
+  var button = document.getElementById('cnet');
+  if (button.style.display === 'none') {
+    button.style.display = 'block';
+  } else {
+    button.style.display = 'none';
+  }
 }
+
+
+
 
 /* $(document).ready(function() {
 
